7370a9e69bb647b1e9a2dc320ebc4e217a6ccbf5
HamiHash <hami.hash@yahoo.com>
2022-12-18 01:42:12 +0330

    readme added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Hero from "./components/Hero/Hero";
 import Layout from "./components/Layout/Layout";
 
